e4605b6ee4a6acd05fd2af4a54b93c2db6ceab8a
kunrenyouAccount <1214kunrenyou@gmail.com>
2021-07-18 17:56:56 +0900

    JetStreamのインストールと設定、トークン管理機能の有効化

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,15 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
-    // return view('welcome');
-    $user = Auth::loginUsingId(1);
-
-    $token = $user->createToken('test');
-
-    dd($token);
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
